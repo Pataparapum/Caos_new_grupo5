f4cfcc5b250e2131e9eaf90402a84064b4423c1e
cristopher <cristophercc67@gmail.com>
2024-05-13 17:48:44 -0400

    añadidio funcionalidad del login

--- Grupo5/js/inicioSesion.js
+++ Grupo5/js/inicioSesion.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
-    $('#form').click(function () {
-        if($('#user').value().length > 40) {
-            
+    $('#form').submit(function () {
+        var user = $('#user').val();
+        if (user.length == 0) {
+            $('#error').text('El usuario no puede estar vacio');
+            event.preventDefault();
+            $('#user').focus();
+
+        } else if (user.length > 20) {
+            $('#error').text('El nombre de usuario no puede tener mas de 20 letras');
+            event.preventDefault();
+            $('#user').focus();
+
+        } else {
+            var email = $('#email').val();
+            if (email.length == 0) {
+                $('#error').text('El email no puede estar vacio');
+                event.preventDefault();
+                $('#email').focus();
+
+            } else {
+                sessionStorage.setItem('usuario',user);
+            }   
+
         }
+
     })
 });
