0d070d2dbc552c501448769d04c3c26a570dbf3d
Freddy <fr.neilaf@duocuc.cl>
2024-06-19 01:08:39 -0400

    hice migracion de noticia climatica

--- caosnoticias/urls.py
+++ caosnoticias/urls.py
@@ -1,7 +1,8 @@
-
+# tu_app_noticias/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('noticias_Climaticas/', views.noticias_Climaticas, name='noticias_Climaticas'),
 ]
--- caosnoticias/views.py
+++ caosnoticias/views.py
@@ -1,8 +1,10 @@
+# tu_app_noticias/views.py
 from django.shortcuts import render
-
-# Create your views here.
 
 def index(request):
     context = {}
-    return render(request, 'noticias/index.html', context)   
+    return render(request, 'noticias/index.html', context)
 
+def noticias_Climaticas(request):
+    context = {}
+    return render(request, 'noticias/noticias_Climaticas.html', context)
