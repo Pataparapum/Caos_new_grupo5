28cc67c4dd95b911b83f1ae3182fc8e0284eb2dd
cristopher <cristophercc67@gmail.com>
2024-06-21 12:32:47 -0400

    añadido app RegisterAndLogin ademas de agregarse en enlace entre el login y el index

--- mipaginaweb/urls.py
+++ mipaginaweb/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('noticias/', include('caosnoticias.urls')),  
+    path('noticias/', include('caosnoticias.urls')),
+    path('sesion/', include('RegisterAndLogin.urls')),
 ]
