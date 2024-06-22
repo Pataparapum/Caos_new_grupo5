87dedfa997fd281043e58232c3b2ff3892186625
cristopher <cristophercc67@gmail.com>
2024-06-22 18:34:41 -0400

    creado crearCuenta.html, y estiloRegistro.css, alterado inicioSesion.html

--- RegisterAndLogin/views.py
+++ RegisterAndLogin/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.+
+def register(request):
+    context = {}
+    return render(request, "crearCuenta.html", context)
+
 def login(request):
     context = {}
     return render(request, "inicioSesion.html", context)
