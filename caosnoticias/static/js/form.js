6530ccd49a80932404ee970de384fc32e521a608
Freddy <fr.neilaf@duocuc.cl>
2024-07-09 19:51:10 -0400

    validaciones contacto

--- caosnoticias/static/js/form.js
+++ caosnoticias/static/js/form.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
             let isValid = true; // Variable para controlar la validación específica de cada campo
             let value = $(this).val().trim(); // Obtener el valor del campo y eliminar espacios en blanco
   
-            // Verificar la validez básica y condiciones específicas
             if (!this.checkValidity() || !isValidInput($(this), value)) {
                 $(this).addClass('is-invalid');
                 $(this).css('border', '2px solid red');
@@ -20,7 +19,8 @@ $(document).ready(function() {
             event.preventDefault();
         }
     });
-  // aqui la clase css para las validaciones
+
+    // aquí la clase CSS para las validaciones
     $('.form-control').on('input', function() {
         let value = $(this).val().trim();
         if (this.checkValidity() && isValidInput($(this), value)) {
@@ -40,14 +40,21 @@ $(document).ready(function() {
             case 'apellidoInput':
                 return value.length >= 2;
             case 'paisInput':
-              case 'ciudadInput':
+            case 'ciudadInput':
                 return value.length >= 3;
             case 'emailInput':
-                return /^[^@]+@[^@]+\.[^@]+$/.test(value); // validar el email
+                return /^[^@]+@[^@]+\.[^@]+$/.test(value); 
             case 'mensajeInput':
                 return value.length >= 10;
+            case 'telefonoInput':
+                return /^\d{10}$/.test(value); 
+            case 'edadInput':
+                let age = parseInt(value, 10);
+                return age >= 18 && age <= 100; // validar la edad entre 18 y 100
+            case 'passwordInput':
+                return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(value); // validar la contraseña con al menos 8 caracteres, una letra y un número
             default:
                 return true; // Si no hay condiciones específicas, se considera válido
         }
     }
-  });
+});
