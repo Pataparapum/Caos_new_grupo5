d066baa2ae195752c0d1b947b4918ecb570b3f87
Freddy <fr.neilaf@duocuc.cl>
2024-07-10 21:43:35 -0400

    mejore el carrito de compras

--- suscripciones/urls.py
+++ suscripciones/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import suscribirse, view_cart, remove_from_cart
+from .views import suscribirse, view_cart, remove_from_cart, realizar_compra, resumen_pedido
 
 urlpatterns = [
     path('suscribirse/', suscribirse, name='suscribirse'),
     path('carrito/', view_cart, name='view_cart'),
     path('carrito/remove/<int:index>/', remove_from_cart, name='remove_from_cart'),
+    path('realizar_compra/', realizar_compra, name='realizar_compra'),
+    path('resumen_pedido/', resumen_pedido, name='resumen_pedido'),
 ]
+
+    
--- suscripciones/views.py
+++ suscripciones/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import SubscriptionForm
+from .forms import SubscriptionForm, UserRegistrationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def suscribirse(request):
     if 'cart' not in request.session:
@@ -9,12 +12,11 @@ def suscribirse(request):
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             subscription_type = form.cleaned_data['subscription_type']
-            type_key = subscription_type.split(' ')[0]
             price = {
-                'mensual': 2500,
-                'semestral': 10000,
-                'anual': 25000,
-            }[type_key]
+                'Plan Plus: Papel + Digital': 40000,
+                'Plan Papel Anual': 32355,
+                'Plan Digital Anual': 10800,
+            }[subscription_type]
             request.session['cart'].append({'type': subscription_type, 'price': price})
             request.session.modified = True
             return redirect('view_cart')
@@ -32,7 +34,7 @@ def view_cart(request):
 
 def remove_from_cart(request, index):
     cart = request.session.get('cart', [])
-    if index < len(cart):
+    if 0 <= index < len(cart):
         del cart[index]
         request.session['cart'] = cart
         request.session.modified = True
@@ -42,3 +44,33 @@ def index(request):
     cart_count = len(request.session.get('cart', []))
     return render(request, 'index.html', {'cart_count': cart_count})
 
+def realizar_compra(request):
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            register_form = UserRegistrationForm(request.POST)
+            if register_form.is_valid():
+                user = register_form.save()
+                user.set_password(register_form.cleaned_data['password'])
+                user.save()
+                login(request, user)
+                return redirect('resumen_pedido')
+        elif 'login' in request.POST:
+            login_form = AuthenticationForm(request, data=request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data.get('username')
+                password = login_form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('resumen_pedido')
+    else:
+        register_form = UserRegistrationForm()
+        login_form = AuthenticationForm()
+    
+    return render(request, 'suscripciones/realizar_compra.html', {'register_form': register_form, 'login_form': login_form})
+
+@login_required
+def resumen_pedido(request):
+    cart = request.session.get('cart', [])
+    total_price = sum(item['price'] for item in cart if isinstance(item, dict))
+    return render(request, 'suscripciones/resumen_pedido.html', {'cart': cart, 'total_price': total_price})
